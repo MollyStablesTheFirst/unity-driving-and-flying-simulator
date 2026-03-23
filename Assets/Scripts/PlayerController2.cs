f3dd1c2ce7b88245df30c9e4872c03adc6c003a0
Molly Stables <molly.stables@etudiant.univ-lr.fr>
2026-03-23 23:04:59 +0100

    Bonus Features 1 - Local multiplayer

--- Assets/Scripts/PlayerController2.cs
+++ Assets/Scripts/PlayerController2.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class PlayerController2 : MonoBehaviour
 {
+    //Private variables
+    private float speed = 20.0f;
+    private float turnspeed = 25.0f;
+    private float horizontalInput;
+    private float verticalInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Player input
+        horizontalInput = Input.GetKeyDown(KeyCode.Q);
+        verticalInput = Input.GetAxis("Vertical");
+        // Move the vehicle forward 
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        // Turn vehicle
+        //transform.Translate(Vector3.right * Time.deltaTime * turnspeed* horizontalInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnspeed * horizontalInput);
     }
 }
